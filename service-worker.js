efd22401efd23bf402ccdace40cb9e5af7de2d76
mineralid <mineralid26@outlook.es>
2026-07-18 15:14:32 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE = 'mineral-id-v3';
+const ASSETS = [
+  './', './index.html', './styles.css', './app.js', './manifest.json', './mineral-id-logo.png'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
